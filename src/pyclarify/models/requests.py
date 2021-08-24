f8eca4969a702e1131ca5948c6a65742689e2297
Eliezer de Souza da Silva <eliezer@searis.no>
2021-08-24 12:23:08 +0200

    test params 2 eliezer

--- src/pyclarify/models/requests.py
+++ src/pyclarify/models/requests.py
@@ -3,6 +3,7 @@ from pydantic.fields import Optional
 from typing import List, Union, Dict
 from datetime import datetime
 from enum import Enum, IntEnum
+from .data import ClarifyDataFrame
 
 
 class ApiMethod(str, Enum):
@@ -11,9 +12,13 @@ class ApiMethod(str, Enum):
     save_signals = 'integration.SaveSignals'
 
 
+class InsertParams(BaseModel):
+    integration: str = ""
+    data: ClarifyDataFrame
+
+
 class JsonRPCRequest(BaseModel):
     jsonrpc: str = "2.0"
     method: ApiMethod = ApiMethod.select
     id: str = "1"
     params: Dict = {}
-
--- tests/models_test.py
+++ tests/models_test.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+import hypothesis
 
 sys.path.append("../src")
 import pyclarify.models as models
@@ -8,19 +9,32 @@ import pyclarify.models as models
 class TestJSONRPC(unittest.TestCase):
     def test_creation(self):
         empty_request = models.requests.JsonRPCRequest()
-        self.assertEqual( empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}' )
+        self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}')
 
     def test_creation_insert(self):
         empty_request = models.requests.JsonRPCRequest(method=models.requests.ApiMethod.insert)
-        self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "integration.Insert", "id": "1", "params": {}}')
+        self.assertEqual(empty_request.json(),
+                         '{"jsonrpc": "2.0", "method": "integration.Insert", "id": "1", "params": {}}')
 
     def test_creation_save(self):
         empty_request = models.requests.JsonRPCRequest(method=models.requests.ApiMethod.save_signals)
-        self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "integration.SaveSignals", "id": "1", "params": {}}')
+        self.assertEqual(empty_request.json(),
+                         '{"jsonrpc": "2.0", "method": "integration.SaveSignals", "id": "1", "params": {}}')
 
-    def test_creation_params(self):
-        empty_request = models.requests.JsonRPCRequest()
-        self.assertEqual( empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}' )
+    def test_creation_params_1(self):
+        empty_request = models.requests.JsonRPCRequest(params={})
+        self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}')
+
+    def test_creation_params_2(self):
+        integration = "test_integration_123"
+        times = ["2021-03-11T21:49:06Z","2021-03-11T21:50:06Z"]
+        values = [0.6, 1.0]
+        signal_id = "test_123_id"
+        data = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+        empty_request = models.requests.JsonRPCRequest(method=models.requests.ApiMethod.insert,
+                                                       params=models.requests.InsertParams(integration=integration,
+                                                                                           data=data))
+        self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "integration.Insert", "id": "1", "params": {"integration": "test_integration_123", "data": {"times": ["2021-03-11T21:49:06+00:00", "2021-03-11T21:50:06+00:00"], "series": {"test_123_id": [0.6, 1.0]}}}}')
 
 
 if __name__ == '__main__':
