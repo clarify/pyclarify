2ae2c5685cd2a9b5ef7cf01151de1bc3ff68f76c
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-08-24 09:09:56 +0200

    possible fix to tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,25 @@
 import unittest
 import sys
 
-sys.path.append("../src")
-import pyclarify.models as models
+sys.path.insert(1, "src/pyclarify")
+from models import data, requests 
 
 
 class TestJSONRPC(unittest.TestCase):
     def test_creation(self):
-        empty_request = models.requests.JsonRPCRequest()
+        empty_request = requests.JsonRPCRequest()
         self.assertEqual( empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}' )
 
     def test_creation_insert(self):
-        empty_request = models.requests.JsonRPCRequest(method=models.requests.ApiMethod.insert)
+        empty_request = requests.JsonRPCRequest(method=requests.ApiMethod.insert)
         self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "integration.Insert", "id": "1", "params": {}}')
 
     def test_creation_save(self):
-        empty_request = models.requests.JsonRPCRequest(method=models.requests.ApiMethod.save_signals)
+        empty_request = requests.JsonRPCRequest(method=requests.ApiMethod.save_signals)
         self.assertEqual(empty_request.json(), '{"jsonrpc": "2.0", "method": "integration.SaveSignals", "id": "1", "params": {}}')
 
     def test_creation_params(self):
-        empty_request = models.requests.JsonRPCRequest()
+        empty_request = requests.JsonRPCRequest()
         self.assertEqual( empty_request.json(), '{"jsonrpc": "2.0", "method": "item.Select", "id": "1", "params": {}}' )
 
 
