2656a3702dc3c7064f25cd1f35d6ae556b60a970
Alexia Baikas <alexia@searis.no>
2021-08-26 11:26:03 +0200

    🚧 Get new token if expired

--- src/pyclarify/oauth2.py
+++ src/pyclarify/oauth2.py
@@ -1,50 +1,34 @@
 import requests
 from models.auth import OAuthResponse, OAuthRequestBody
 import json
+import datetime
 
 class GetToken():
 
-    def __init__(
-        self, 
-        access_token=None,
-        ):
-        """
-        [summary]
-
-        Parameters
-        ----------
-        client_id : [type]
-            [description]
-        client_secret : [type]
-            [description]
-        access_token : [type], optional
-            [description], by default None
-        """
-        self._access_token = access_token
-
+    def __init__(self):
+        self.access_token = None
+        self._expire_date = datetime.datetime.now()
+        self._expire_token = None
 
     def load_file(self):
-        """
-        [summary]
-
-        Returns
-        -------
-        [type]
-            [description]
-        """
         with open("clarify-credentials.json") as f:
             data = json.load(f)
             oauth_request_obj = OAuthRequestBody(client_id=data["credentials"]["clientId"],
                                                 client_secret=data["credentials"]["clientSecret"])
-        return oauth_request_obj
+        return oauth_request_obj.dict()
 
-    def get_token(self, url:str,):
+    def new_token(self, url:str):
         data = self.load_file()
-        print(dict(data))
-        response = requests.post(url, dict(data))
+        response = requests.post(url, data)
+        token_obj = OAuthResponse(**response.json())
+        self._expire_token = self._expire_date + token_obj.dict()["expires_in"]
+        self.access_token = token_obj.dict()["access_token"]
 
-        if response.ok:
-            token_obj = OAuthResponse(**response.json())
-        else:
-            print(response.content)
-        return token_obj
+        return self.access_token
+
+    def get_token(self, url:str):
+        if self._expire_token == None:
+            self.new_token(url)
+
+        elif self._expire_token > self._expire_date:
+            return self.access_token
