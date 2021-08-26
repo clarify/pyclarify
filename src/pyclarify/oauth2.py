7033ef7f538a1987ba7f6d39edddb28c4dca7dbf
Alexia Baikas <alexia@searis.no>
2021-08-26 14:12:27 +0200

    ✨ Get user token

--- src/pyclarify/oauth2.py
+++ src/pyclarify/oauth2.py
@@ -1,34 +1,72 @@
 import requests
-from models.auth import OAuthResponse, OAuthRequestBody
-import json
+from models.auth import OAuthResponse, OAuthRequestBody, ClarifyCredential
 import datetime
 
-class GetToken():
 
-    def __init__(self):
+class GetToken:
+    def __init__(self, url, credential_file: ClarifyCredential):
+
         self.access_token = None
+        self.content_type = "application/x-www-form-urlencoded"
+        self.credential_file = credential_file
         self._expire_date = datetime.datetime.now()
         self._expire_token = None
+        self.url = url
 
-    def load_file(self):
-        with open("clarify-credentials.json") as f:
-            data = json.load(f)
-            oauth_request_obj = OAuthRequestBody(client_id=data["credentials"]["clientId"],
-                                                client_secret=data["credentials"]["clientSecret"])
-        return oauth_request_obj.dict()
+    def read_credentials(self):
+        """
+        Read user credentials.
 
-    def new_token(self, url:str):
-        data = self.load_file()
-        response = requests.post(url, data)
+        Returns
+        -------
+        dict
+            Dictionary of the user credentials.
+        """
+        data = self.credential_file
+        oauth_request_body = OAuthRequestBody(
+            client_id=data["credentials"]["clientId"],
+            client_secret=data["credentials"]["clientSecret"],
+        )
+        return oauth_request_body.dict()
+
+    def new_token(self, url: str):
+        """
+        Get a new token using the users credentials.
+
+        Parameters
+        ----------
+        url : str, default "https://login.clarify.us/oauth/token"
+            URL to make the post request to the Clarify API.
+
+        Returns
+        -------
+        str
+            User token.
+        """
+        data = self.read_credentials()
+        response = requests.post(
+            url=url,
+            headers={"content-type": self.content_type},
+            data=data,
+        )
+
         token_obj = OAuthResponse(**response.json())
-        self._expire_token = self._expire_date + token_obj.dict()["expires_in"]
-        self.access_token = token_obj.dict()["access_token"]
-
+        self._expire_token = self._expire_date + token_obj.expires_in
+        self.access_token = token_obj.access_token
         return self.access_token
 
-    def get_token(self, url:str):
-        if self._expire_token == None:
-            self.new_token(url)
+    def get_token(self):
+        """
+        Check if token exists or has expired, if yes get a new one, else return the old one.
 
-        elif self._expire_token > self._expire_date:
+        Returns
+        -------
+        str
+            User token.
+        """
+        if (self._expire_token == None) or (
+            self._expire_token <= datetime.datetime.now()
+        ):
+            self.new_token(self.url)
+        elif self._expire_token > datetime.datetime.now():
             return self.access_token
