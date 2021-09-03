fe641d4c6f0f082c68398f540b0003ab9a110739
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-09-03 14:38:49 +0200

    :recycle: remove blank lines

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -84,7 +84,7 @@ class TestBase(unittest.TestCase):
 
         # assert is correct id
         self.assertEqual(response["id"], payload["id"])
-    
+
     def test_authentication(self):
         read = self.interface.authenticate("./tests/test-clarify-credentials.json")
         self.assertTrue(read)
