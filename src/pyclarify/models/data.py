77b88ac706c749f8de05f5117cd429e293f0eb30
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-09-15 13:37:54 +0200

    :sparkles: add merge util method for Clarify Data Frames (#26)
    Implement merge method in /model/data.py for merging two or more Clarify Data Frames.

--- src/pyclarify/models/data.py
+++ src/pyclarify/models/data.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel, constr, validate_arguments
 from pydantic.fields import Optional
 from typing import List, Union, Dict
 from datetime import datetime, timedelta
+import logging
 from enum import Enum
 
 # constrained string defined by the API
@@ -14,6 +15,47 @@ NumericalValuesType = List[Union[float, int, None]]
 class ClarifyDataFrame(BaseModel):
     times: List[datetime] = None
     series: Dict[InputId, NumericalValuesType] = None
+
+@validate_arguments
+def merge(dataframes : List[ClarifyDataFrame]):
+    """
+    Method for merging 2 or more Clarify Data Frames. Mapping overlapping 
+    signal names to single series. Concatenates timestamps of all data frames. 
+    Inserts none value to series not containing entry at a given timestamp.
+
+    Parameters
+    ----------
+    dataframes : List[ClarifyDataFrame]
+       List of Clarify Data Frames
+
+    Returns
+    -------
+    ClarifyDataFrame : ClarifyDataFrame
+        returns the wrapped function 
+    """
+    signals = [key for df in dataframes for key in df.series.keys()]
+    signals = list(set(signals))
+
+    cdf_dict = {}
+    for cdf in dataframes:
+        for signal, values in list(cdf.series.items()):
+            for value, time in zip(values, cdf.times):
+                cdf_dict.setdefault(time,[]).append((signal, value))
+
+    times = sorted(list(cdf_dict.keys()))
+
+    # make sure not to refrence pointers
+    signal_values = [[None]*len(times) for i in range(len(signals))] 
+
+    for i, time in enumerate(times):
+        for value in cdf_dict[time]:
+            signal_values[signals.index(value[0])][i] = value[1]
+
+    series = {}
+    for signal, values in zip(signals, signal_values):
+        series[signal] = values
+
+    return ClarifyDataFrame(times=times, series=series)
 
 
 class TypeSignal(str, Enum):
--- tests/test_models.py
+++ tests/test_models.py
@@ -81,6 +81,77 @@ class TestModels(unittest.TestCase):
             '{"jsonrpc": "2.0", "method": "integration.Insert", "id": "1", "params": {"integration": "01234567890123456789", "data": {"times": ["2021-03-11T21:49:06+00:00", "2021-03-11T21:50:06+00:00"], "series": {"test_123_id": [0.6, 1.0]}}}}',
         )
 
+    def test_merge_single_input(self):
+        times = ["2021-03-11T21:49:06Z", "2021-03-11T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_1"
+        cdf = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        merged = models.data.merge([cdf])
+
+        # expect merged to be same as single input
+        self.assertEqual(merged, cdf)
+
+
+    def test_merge_two_inputs(self):
+        times = ["2021-03-11T21:49:06Z", "2021-03-11T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_1"
+        cdf = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        times = ["2021-03-10T21:49:06Z", "2021-03-10T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.3, 2.0, 3.5]
+        signal_id = "signal_2"
+        cdf2 = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        merged = models.data.merge([cdf, cdf2])
+        merged_reverse = models.data.merge([cdf2, cdf])
+
+        self.assertEqual(merged, merged_reverse)
+    
+    def test_merge_multiple_inputs(self):
+        times = ["2021-03-11T21:49:06Z", "2021-03-11T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_1"
+        cdf = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        times = ["2021-03-10T21:49:06Z", "2021-03-10T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.3, 2.0, 3.5]
+        signal_id = "signal_2"
+        cdf2 = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        times = ["2021-03-09T21:49:06Z", "2021-03-09T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_3"
+        cdf3 = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+        
+        merged = models.data.merge([cdf, cdf2, cdf3])
+        merged2 = models.data.merge([cdf, cdf3, cdf2])
+        merged3 = models.data.merge([cdf2, cdf, cdf3])
+        merged4 = models.data.merge([cdf2, cdf3, cdf])
+        merged5 = models.data.merge([cdf3, cdf, cdf2])
+        merged6 = models.data.merge([cdf3, cdf2, cdf])
+        
+        self.assertEqual(merged, merged2)
+        self.assertEqual(merged, merged3)
+        self.assertEqual(merged, merged4)
+        self.assertEqual(merged, merged5)
+        self.assertEqual(merged, merged6)
+
+    def test_merge_equal_input(self):
+        times = ["2021-03-11T21:49:06Z", "2021-03-11T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_1"
+        cdf = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        times = ["2021-03-11T21:49:06Z", "2021-03-11T21:50:06Z", "2021-03-11T21:51:06Z"]
+        values = [0.6, 1.0, 2.4]
+        signal_id = "signal_1"
+        cdf2 = models.data.ClarifyDataFrame(times=times, series={signal_id: values})
+
+        merged = models.data.merge([cdf,cdf2])
+
+        self.assertEqual(merged, cdf)
 
 if __name__ == "__main__":
     unittest.main()
