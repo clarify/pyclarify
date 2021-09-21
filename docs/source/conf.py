fa6f20ee015a4597b9d348c901c6ed9d647594e5
Alexia Baikas <alexia@clarify.io>
2021-09-21 14:55:27 +0200

    Update extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,8 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc'
 ]
 
 napoleon_google_docstring = False
