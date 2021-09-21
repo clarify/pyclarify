82672dcfb081a5df9d3f46e709445c4f21e744dc
Eliezer Silva <eliezer@searis.no>
2021-09-21 13:27:58 +0200

    Fix new api urls :ambulance: (#36)
    * fix to make all links on the client based on the links on credentials.json file. This is necessary for both current updates in the oauth and API url, as well as future updates.
    
    * change in Auth exception handling

--- src/pyclarify/oauth2.py
+++ src/pyclarify/oauth2.py
@@ -25,11 +25,13 @@ class GetToken:
             The path to the clarify_credentials.json downloaded from the Clarify app,
             or json/dictionary of the content in clarify_credentials.json
         """
+        self.api_url = None
         self.access_token = None
         self.integration_id = None
         self.headers = {"content-type": "application/x-www-form-urlencoded"}
         self.credentials = self.read_credentials(clarify_credentials)
-        self.auth_endpoint = "https://login.clarify.us/oauth/token"
+        self.api_url = self.credentials.audience
+        self.auth_endpoint = f"{self.api_url}oauth/token"
         self._expire_token = None
 
     def read_credentials(self, clarify_credentials):
@@ -50,17 +52,10 @@ class GetToken:
         """
         if isinstance(clarify_credentials, str):
             if path.exists(clarify_credentials):
-                f = open(clarify_credentials)
-                clarify_credentials_object = json.load(f)
-                f.close()
+                with open(clarify_credentials) as f:
+                    clarify_credentials = json.load(f)
             else:
-                try:
-                    clarify_credentials_object = json.loads(clarify_credentials)
-                except:
-                    logging.error(
-                        f"{clarify_credentials} is of type string, but is not a valid path or credentials"
-                    )
-                    return False
+                clarify_credentials = json.loads(clarify_credentials)
 
         if isinstance(clarify_credentials, dict):
             clarify_credentials_object = clarify_credentials
@@ -83,13 +78,18 @@ class GetToken:
             Access token.
         """
         response = requests.post(
-            url=self.auth_endpoint, headers=self.headers, data=self.credentials.dict(),
+            url=self.auth_endpoint,
+            headers=self.headers,
+            data=self.credentials.dict(),
         )
 
-        token_obj = OAuthResponse(**response.json())
-        self._expire_token = datetime.datetime.now() + token_obj.expires_in
-        self.access_token = token_obj.access_token
-        return self.access_token
+        if response.ok:
+            token_obj = OAuthResponse(**response.json())
+            self._expire_token = datetime.datetime.now() + token_obj.expires_in
+            self.access_token = token_obj.access_token
+            return self.access_token
+        else:
+            raise AuthError(**response.json())
 
     def get_token(self):
         """
@@ -100,9 +100,24 @@ class GetToken:
         str
             Access token.
         """
-        if (self._expire_token == None) or (
+        if (self._expire_token is None) or (
             self._expire_token <= datetime.datetime.now()
         ):
             return self.get_new_token()
         elif self._expire_token > datetime.datetime.now():
             return self.access_token
+
+
+class AuthError(Exception):
+    """
+    Error class that is generated when an authentication error appear
+    """
+
+    def __init__(self, error, error_description):
+        self.error = error
+        self.error_description = error_description
+
+    def __str__(self):
+        return (
+            f"Authentication error: {self.error}. Description: {self.error_description}"
+        )
