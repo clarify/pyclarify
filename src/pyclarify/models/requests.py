a229d6657d3befdc74d12886b050cce28d80dd75
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2021-11-22 15:44:53 +0100

    Version 0.2.0 (#60)
    * Add Admin API
    
    * Add Select Signal method
    
    - add method for selecting signals
    - Splitting Signal class into Signal and SignalInfo
    - Add test suite for select signal and reworking other test cases to fit splitting of Signal class
    
    * Add update rollup and add forbid extra attributes during model initialisation 
    
    * Add publish signal method with tests 
    
    * Include typing_extensions in setup.cfg 
    
    
    * Change license to Apache v2.0
    
    * Restructure internal models
    
    - Remove all exposed models, except DataFrame
    - Rewrite all tests
    - Split internal models into Request/Response and move datatypes to data module
    
    * Update Documentation
    
    * :bookmark: bump version to 0.2.0
    
    Co-authored-by: Alexia Artemis Baikas <46658231+AlexiaArtemis@users.noreply.github.com>

--- src/pyclarify/models/requests.py
+++ src/pyclarify/models/requests.py
@@ -1,25 +1,123 @@
-from pydantic import BaseModel, constr, conint
+"""
+Copyright 2021 Clarify
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from pydantic import BaseModel, constr, conint, Extra, validate_arguments
 from pydantic.fields import Optional
+import pydantic
 from typing import List, Union, Dict
+from typing_extensions import Literal
 from datetime import datetime
 from enum import Enum
-from .data import DataFrame, InputID, Signal
+from .data import DataFrame, InputID, SignalInfo, Signal
 from pyclarify.__utils__.convert import timedelta_isoformat
 from datetime import timedelta
 
 IntegrationID = constr(regex=r"^[a-v0-9]{20}$")
-LimitSelect = conint(ge=0, le=20)
+ResourceID = constr(regex=r"^[a-v0-9]{20}$")
+LimitSelectItems = conint(ge=0, le=50)
+LimitSelectSignals = conint(ge=0, le=1000)
+
+### PARAMETERS ###
+
+# Generic Query Parameters #
+class QueryParams(BaseModel):
+    include: Optional[bool] = False
+    filter: dict = {}
+    skip: Optional[int] = 0
 
 
+class SelectSignalsSignalsParams(QueryParams, extra=Extra.forbid):
+    limit: Optional[LimitSelectSignals] = 50
+
+
+class SelectItemsItemsParams(QueryParams, extra=Extra.forbid):
+    limit: Optional[LimitSelectItems] = 10
+
+
+# Generic Inclusion Parameters #
+class InclusionParams(BaseModel):
+    include: Optional[bool] = False
+
+
+class SelectItemsDataParams(InclusionParams, extra=Extra.forbid):
+    notBefore: Optional[datetime]
+    before: Optional[datetime]
+    rollup: Optional[Union[timedelta, Literal["window"]]]
+
+
+class SelectSignalsItemsParams(InclusionParams, extra=Extra.forbid):
+    pass
+
+
+# Generic Request Parameter
+class RequestParams(BaseModel):
+    pass
+
+
+# Clarify Namespace
+class ClarifyParams(RequestParams):
+    pass
+
+
+class SelectItemsParams(ClarifyParams, extra=Extra.forbid):
+    items: SelectItemsItemsParams
+    data: SelectItemsDataParams
+
+
+# Integration Namespace
+class IntegrationParams(RequestParams):
+    integration: IntegrationID
+
+
+class InsertParams(IntegrationParams):
+    data: DataFrame
+
+
+class SaveSignalsParams(IntegrationParams, extra=Extra.forbid):
+    inputs: Dict[InputID, SignalInfo]
+    createOnly: Optional[bool] = False
+
+
+# Admin Namespace
+class AdminParams(RequestParams):
+    integration: IntegrationID
+
+
+class PublishSignalsParams(AdminParams):
+    itemsBySignal: Dict[ResourceID, SignalInfo]
+    createOnly: Optional[bool] = False
+
+
+class SelectSignalsParams(AdminParams):
+    signals: SelectSignalsSignalsParams
+    items: SelectSignalsItemsParams
+
+
+### REQUESTS ###
 class ApiMethod(str, Enum):
-    select = "clarify.SelectItems"
     insert = "integration.Insert"
     save_signals = "integration.SaveSignals"
+    select_items = "clarify.SelectItems"
+    select_signals = "admin.SelectSignals"
+    publish_signals = "admin.PublishSignals"
 
 
 class JSONRPCRequest(BaseModel):
     jsonrpc: str = "2.0"
-    method: ApiMethod = ApiMethod.select
+    method: ApiMethod = ApiMethod.select_items
     id: str = "1"
     params: Dict = {}
 
@@ -27,92 +125,26 @@ class JSONRPCRequest(BaseModel):
         json_encoders = {timedelta: timedelta_isoformat}
 
 
-class InsertParams(BaseModel):
-    integration: IntegrationID
-    data: DataFrame
+@validate_arguments
+class Request(JSONRPCRequest):
+    method: ApiMethod
 
-
-class InsertRequest(JSONRPCRequest):
-    method: ApiMethod = ApiMethod.insert
-    params: InsertParams
-
-
-class SaveParams(BaseModel):
-    integration: IntegrationID
-    inputs: Dict[InputID, Signal]
-    createdOnly: Optional[bool] = False
-
-
-class SaveRequest(JSONRPCRequest):
-    method: ApiMethod = ApiMethod.save_signals
-    params: SaveParams
-
-
-class ErrorData(BaseModel):
-    trace: str
-    params: Optional[Dict[str, List[str]]]
-
-
-class Error(BaseModel):
-    code: str
-    message: str
-    data: Optional[Union[ErrorData, str]]
-
-
-class GenericResponse(BaseModel):
-    jsonrpc: str = "2.0"
-    id: Optional[str]
-    result: Optional[Dict]
-    error: Optional[Error]
-
-
-class GenericSummary(BaseModel):
-    id: str
-    created: bool
-
-class InsertSummary(GenericSummary):
-    pass
-
-class SaveSummary(GenericSummary):
-    updated: bool
-
-
-class SignalSaveMap(BaseModel):
-    signalsByInput: Dict[InputID, Union[SaveSummary, InsertSummary]]
-
-
-class SaveResponse(GenericResponse):
-    result: Optional[SignalSaveMap]
-
-
-class SelectItemsParams(BaseModel):
-    include: Optional[bool] = False
-    filter: dict = {}                   # https://docs.clarify.io/v1.1/reference/filtering
-    limit: Optional[LimitSelect] = 10
-    skip: Optional[int] = 0
-
-
-class SelectDataParams(BaseModel):
-    include: Optional[bool] = False
-    notBefore: Optional[datetime]
-    before: Optional[datetime]
-    rollup: Optional[timedelta] = None
-
-
-class ItemSelect(BaseModel):
-    items: SelectItemsParams
-    data: SelectDataParams
-
-
-class SelectRequest(JSONRPCRequest):
-    method: ApiMethod = ApiMethod.select
-    params: ItemSelect
-
-
-class SelectMapResult(BaseModel):
-    items: Optional[Dict[InputID, Signal]]
-    data: Optional[DataFrame]
-
-
-class SelectResponse(GenericResponse):
-    result: Optional[SelectMapResult]
+    @pydantic.root_validator()
+    @classmethod
+    def use_correct_params_based_on_method(cls, values):
+        if values["method"] == ApiMethod.insert:
+            values["params"] = InsertParams(**values["params"])
+            return values
+        elif values["method"] == ApiMethod.save_signals:
+            values["params"] = SaveSignalsParams(**values["params"])
+            return values
+        elif values["method"] == ApiMethod.select_items:
+            values["params"] = SelectItemsParams(**values["params"])
+            return values
+        elif values["method"] == ApiMethod.select_signals:
+            values["params"] = SelectSignalsParams(**values["params"])
+            return values
+        elif values["method"] == ApiMethod.publish_signals:
+            values["params"] = PublishSignalsParams(**values["params"])
+            return values
+        return values
