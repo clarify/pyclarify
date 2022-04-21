2e17b5b55f9834ab5a032ed8d9973cb4a146a4d7
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2022-04-21 15:19:09 +0200

    Ds 45 refactor pagination (#87)
    * refactor iterators
    
    * extended Response to handle add operation and refactored return on all methods
    
    * add tests for rfc3339_to_timedelta

--- src/pyclarify/models/requests.py
+++ src/pyclarify/models/requests.py
@@ -22,8 +22,8 @@ from typing_extensions import Literal
 from datetime import datetime
 from enum import Enum
 from .data import DataFrame, InputID, SignalInfo, Signal, Item
-from pyclarify.__utils__.convert import timedelta_isoformat, time_to_string
-from pyclarify.__utils__.pagination import GetDates
+from pyclarify.__utils__.time import timedelta_isoformat, time_to_string
+from pyclarify.__utils__.pagination import TimeIterator
 from datetime import timedelta
 
 IntegrationID = constr(regex=r"^[a-v0-9]{20}$")
--- tests/test_pagination.py
+++ tests/test_pagination.py
@@ -2,25 +2,38 @@ import unittest
 import sys
 
 sys.path.insert(1, "src/")
-from pyclarify.__utils__.pagination import GetDates, GetItems
+from pyclarify.__utils__.pagination import TimeIterator, ItemIterator
 import datetime
 
 
 class TestPagination(unittest.TestCase):
-    def test_get_items(self):
+    def test_item_iterator(self):
 
-        items = GetItems(50, 123, 45)
+        items = ItemIterator(user_limit=123, limit_per_call=50, skip=45)
         item_iter = iter(items)
 
-        self.assertEqual(next(item_iter), (50, 45))
-        self.assertEqual(next(item_iter), (50, 95))
-        self.assertEqual(next(item_iter), (23, 145))
+        self.assertEqual(next(item_iter), (45, 50))
+        self.assertEqual(next(item_iter), (95, 50))
+        self.assertEqual(next(item_iter), (145, 23))
         with self.assertRaises(StopIteration):
             next(item_iter)
+    def test_in_for_loop(self):
+        i = 0
+        for skip, limit in ItemIterator(user_limit=123, limit_per_call=50, skip=45):
+            if i == 0:
+                self.assertEqual(skip, 45)
+                self.assertEqual(limit, 50)
+            if i == 1:
+                self.assertEqual(skip, 95)
+                self.assertEqual(limit, 50)
+            if i == 2:
+                self.assertEqual(skip, 145)
+                self.assertEqual(limit, 23)
+            i += 1
+    
+    def test_time_iterator(self):
 
-    def test_get_dates(self):
-
-        dates = GetDates(["2020-10-06T17:48:04Z", "2021-01-10T21:50:06Z"])
+        dates = TimeIterator(start_time="2020-10-06T17:48:04Z", end_time="2021-01-10T21:50:06Z")
         dates_iter = iter(dates)
 
         self.assertEqual(
@@ -33,20 +46,35 @@ class TestPagination(unittest.TestCase):
         self.assertEqual(
             next(dates_iter),
             (
-                datetime.datetime(2020, 11, 15, 17, 48, 5),
-                datetime.datetime(2020, 12, 25, 17, 48, 5),
+                datetime.datetime(2020, 11, 15, 17, 48, 4),
+                datetime.datetime(2020, 12, 25, 17, 48, 4),
             ),
         )
         self.assertEqual(
             next(dates_iter),
             (
-                datetime.datetime(2020, 12, 25, 17, 48, 6),
+                datetime.datetime(2020, 12, 25, 17, 48, 4),
                 datetime.datetime(2021, 1, 10, 21, 50, 6),
             ),
         )
         with self.assertRaises(StopIteration):
             next(dates_iter)
+    
+    def test_time_iterator_rollup(self):
 
+        dates = TimeIterator(start_time="2020-10-06T17:48:04Z", end_time="2021-01-10T21:50:06Z", rollup="P1DT")
+        dates_iter = iter(dates)
+
+        self.assertEqual(
+            next(dates_iter),
+            (
+                datetime.datetime(2020, 10, 6, 17, 48, 4),
+                datetime.datetime(2021, 1, 10, 21, 50, 6),
+            ),
+        )
+        
+        with self.assertRaises(StopIteration):
+            next(dates_iter)
 
 if __name__ == "__main__":
     unittest.main()
