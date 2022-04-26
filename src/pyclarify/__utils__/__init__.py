cf029c6ed22d00e43368fcab67885d67ab8c4027
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2022-04-26 10:35:58 +0200

    add editorconfig and update license

--- src/pyclarify/__utils__/__init__.py
+++ src/pyclarify/__utils__/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 Clarify
+Copyright 2022 Searis AS
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
