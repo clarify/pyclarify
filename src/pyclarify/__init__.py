986896d15e11245cf5203dce923c15ac2ec07c05
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2023-08-09 14:44:00 +0200

    Version 0.4.5

--- src/pyclarify/__init__.py
+++ src/pyclarify/__init__.py
@@ -18,5 +18,5 @@ from pyclarify.client import Client
 from pyclarify.views import Signal, SignalInfo, Item, ItemInfo, DataFrame
 import pyclarify.query
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __API_version__ = "1.1beta2"
