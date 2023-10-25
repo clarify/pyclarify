995513bce20d62540756cc0a47ff3fa1bf679d6e
Odd Gunnar Aspaas <odd.gunnar.aspaas@gmail.com>
2023-10-25 12:17:21 +0200

    update docs

--- src/pyclarify/views/evaluate.py
+++ src/pyclarify/views/evaluate.py
@@ -25,8 +25,55 @@ from pyclarify.query.query import DataQuery
 
 class ItemAggregation(BaseModel):
     """
-    :meta private:
-    """
+        Model for creating item aggregations to be used in evaluate endpoint.
+
+        Parameters
+        ----------
+
+        id: ResourceID (r"^[a-v0-9]{20}$")
+            The ID of the item to be aggregated.
+
+        aggregation: str
+            The aggregation type to be done. Current legal aggregations are found `here <https://docs.clarify.io/api/1.1/types/fields#data-aggregation>`__. 
+
+        state: int[0:9999]
+            The integer denoting the state to be used in the aggregation. Only necessary when using state based aggregation.
+
+        lead: int[-1000:1000]
+            Shift buckets backwards by N.
+
+        lag: int[-1000:1000]
+            Shift buckets forwards by N.
+
+        alias: string
+            A short alias to use in formulas as well as in the data frame results.
+
+        
+        Example
+        -------
+        
+            >>> from pyclarify import ItemAggregation
+        
+            Creating a signal a minimal item aggregation.
+
+            >>> itemAgg = ItemAggregation(
+            ...     id="cbpmaq6rpn52969vfl0g",
+            ...     aggregation="avg",
+            ...     alias="i2"
+            ... )
+
+            Creating a item aggregation with all attributes set.
+
+            >>> item = ItemAggregation(
+            ...     id="cbpmaq6rpn52969vfl00",
+            ...     aggregation="max",
+            ...     state=1,
+            ...     lead=1,
+            ...     lag=1,
+            ...     alias="i1"
+            ... )
+        """
+
     id: ResourceID
     aggregation: DataAggregation
     state: Optional[State]
@@ -38,7 +85,40 @@ class ItemAggregation(BaseModel):
 
 class Calculation(BaseModel):
     """
-    :meta private:
+        Model for creating a calculation to be used in evaluate endpoint.
+
+        Parameters
+        ----------
+
+        formula: str
+            The formula to be applied. Current legal calculations are found `here <https://docs.clarify.io/users/admin/items/calculated-items#formula>`__. 
+
+        alias: string
+            A short alias to use in formulas as well as in the data frame results.
+
+         
+        Example
+        -------
+        
+            >>> from pyclarify import Calculation
+        
+            Creating a calculation assuming we have items with aliases "i1" and "i2".
+
+            >>> calc = Calculation(
+            ...     formula="i1 + i2"
+            ...     alias="c1"
+            ... )
+
+            Creating a calculation using other calculations.
+
+            >>> calc1 = Calculation(
+            ...     formula="i1 + i2"
+            ...     alias="c1"
+            ... )
+            >>> calc2 = Calculation(
+            ...     formula="c1**2 + i1"
+            ...     alias="c1"
+            ... )
     """
     #TODO: constraints for formula?
     formula: str
